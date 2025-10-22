92b37351e08baef94f7cc9611b05552b454f1895
sandhy <sandhy.putra98@gmail.com>
2025-10-22 17:05:51 +0700

    update sandhy : tambah preview foto gallery

--- slider.js
+++ slider.js
@@ -5,11 +5,19 @@ let index = 0;
 const visible = 3; // tampil 3 gambar per frame
 let autoSlide;
 
+// Update slide + animasi fade
 function updateSlide() {
   const width = images[0].clientWidth + 10; // +gap
   slider.style.transform = `translateX(${-index * width}px)`;
+
+  // fade in animasi
+  images.forEach(img => img.classList.remove('show'));
+  for (let i = index; i < index + visible; i++) {
+    if (images[i]) images[i].classList.add('show');
+  }
 }
 
+// Navigasi
 function nextSlide() {
   if (index < totalImages - visible) index++;
   else index = 0;
@@ -22,13 +30,15 @@ function prevSlide() {
   updateSlide();
 }
 
+// Auto slide control
 function startAutoSlide() {
-  autoSlide = setInterval(nextSlide, 3000);
+  autoSlide = setInterval(nextSlide, 5000);
 }
 function stopAutoSlide() {
   clearInterval(autoSlide);
 }
 
+// Tombol manual
 document.querySelector('.slide-btn.next').addEventListener('click', () => {
   stopAutoSlide(); nextSlide(); startAutoSlide();
 });
@@ -45,5 +55,30 @@ slider.addEventListener('touchend', e => {
   else if (endX - startX > 50) { stopAutoSlide(); prevSlide(); startAutoSlide(); }
 });
 
-// Start auto-slide
+// Pause auto saat hover
+slider.addEventListener('mouseenter', stopAutoSlide);
+slider.addEventListener('mouseleave', startAutoSlide);
+
+// Lightbox
+const lightbox = document.createElement('div');
+lightbox.id = 'lightbox';
+const lightboxImg = document.createElement('img');
+lightbox.appendChild(lightboxImg);
+document.body.appendChild(lightbox);
+
+images.forEach(img => {
+  img.addEventListener('click', () => {
+    lightboxImg.src = img.src;
+    lightbox.style.display = 'flex';
+    stopAutoSlide();
+  });
+});
+
+lightbox.addEventListener('click', () => {
+  lightbox.style.display = 'none';
+  startAutoSlide();
+});
+
+// Mulai
+updateSlide();
 startAutoSlide();
